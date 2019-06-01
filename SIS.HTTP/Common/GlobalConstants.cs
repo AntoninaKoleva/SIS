506f8f753e75ecb97edc9accc41fc7cbab9f99b2
Antonina <antonina.koleva1@gmail.com>
2019-06-01 20:34:23 +0300

    Added UnsupportedHttpMethodException.

--- SIS.HTTP/Common/GlobalConstants.cs
+++ SIS.HTTP/Common/GlobalConstants.cs
@@ -3,7 +3,11 @@
     public class GlobalConstants
     {
         public const string HttpOneProtocolFragment = "HTTP/1.1";
+
         public const string HostHeaderString = "Host";
+
         public const string HttpNewLine = "\r\n";
+
+        public const string UnsupportedHttpMethodException = "This HTTP method - {0} is not supported!";
     }
 }
