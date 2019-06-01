f02fefad22a75466eb34f827a71c692d17f30ae2
Antonina <antonina.koleva1@gmail.com>
2019-06-01 20:34:07 +0300

    Fixed typo.

---
+++ SIS.HTTP/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public const string BadRequestExceptionDefaultMessage = 
+            "The Request was malformed or contains unsupported elements.";
+        public BadRequestException() 
+            : this(BadRequestExceptionDefaultMessage)
+        {
+
+        }
+        public BadRequestException(string name) : base(name)
+        {
+
+        }
+    }
+}
