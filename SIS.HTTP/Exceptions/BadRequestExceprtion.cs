550697e5d4a16e73e3d63258a990e65584b821ae
Antonina <antonina.koleva1@gmail.com>
2019-06-01 14:28:31 +0300

    Exceptions

---
+++ SIS.HTTP/Exceptions/BadRequestExceprtion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class BadRequestExceprtion : Exception
+    {
+        public const string BadRequestExceptionDefaultMessage = 
+            "The Request was malformed or contains unsupported elements.";
+        public BadRequestExceprtion() 
+            : this(BadRequestExceptionDefaultMessage)
+        {
+
+        }
+        public BadRequestExceprtion(string name) : base(name)
+        {
+
+        }
+    }
+}
