ba8f1c15c9f1184dd30420ed5802574a201b0dc1
Antonina <antonina.koleva1@gmail.com>
2019-06-01 20:35:45 +0300

    Added HttpHeaderCollection.

---
+++ SIS.HTTP/Header/HttpHeaderCollection.cs
@@ -0,0 +1,41 @@
+﻿using SIS.HTTP.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SIS.HTTP.Header
+{
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> httpHeaders;
+
+        public HttpHeaderCollection()
+        {
+            this.httpHeaders = new Dictionary<string, HttpHeader>();
+        }
+
+        public void AddHeader(HttpHeader header)
+        {
+            CoreValidator.ThrowIfNull(header, nameof(header));
+            this.httpHeaders.Add(header.Key, header);
+        }
+
+        public bool CotainsHeader(string key)
+        {
+            CoreValidator.ThrowIfNull(key, nameof(key));
+            return this.httpHeaders.ContainsKey(key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            CoreValidator.ThrowIfNull(key, nameof(key));
+            return this.httpHeaders[key];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(GlobalConstants.HttpNewLine, 
+                this.httpHeaders.Values.Select(h => h.ToString()))  ;
+        }
+
+    }
+}
---
+++ SIS.HTTP/Header/IHttpHeaderCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace SIS.HTTP.Header
+{
+    public interface IHttpHeaderCollection
+    {
+        void AddHeader(HttpHeader header);
+        bool CotainsHeader(string key);
+        HttpHeader GetHeader(string key);
+    }
+}
