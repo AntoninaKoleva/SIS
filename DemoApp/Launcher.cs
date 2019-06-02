e36e08f7adc1e4febf3ed7fe3efdb51c29c5239b
Antonina <antonina.koleva1@gmail.com>
2019-06-02 18:04:36 +0300

    HomeController and Launcher.

---
+++ DemoApp/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using SIS.HTTP.Enums;
+using SIS.WebServer;
+using SIS.WebServer.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            var serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(
+                HttpRequestMethod.GET, 
+                "/", 
+                request => new HomeController().Index(request));
+
+            Server server = new Server(port: 8000, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
