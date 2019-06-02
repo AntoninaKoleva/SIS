32df38bbf5354341448ce5b06fe3c4dda57b33b1
Antonina <antonina.koleva1@gmail.com>
2019-06-02 17:23:15 +0300

    Created ServerRoutingTable

---
+++ SIS.WebServer/Routing/IServerRoutingTable.cs
@@ -0,0 +1,15 @@
+﻿using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using System;
+
+namespace SIS.WebServer.Routing
+{
+    public interface IServerRoutingTable
+    {
+        void Add(HttpRequestMethod requestMethod, string path, Func<IHttpRequest, IHttpResponse> funk);
+        bool Contains(HttpRequestMethod requestMethod, string path);
+        Func<IHttpRequest, IHttpResponse> Get(HttpRequestMethod requestMethod, string path);
+
+    }
+}
