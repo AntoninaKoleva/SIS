a36c822c2da35150a056d89469059bf3d869c86a
Antonina <antonina.koleva1@gmail.com>
2019-06-02 17:23:31 +0300

    ConnectionHandler

---
+++ SIS.WebServer/ConnectionHandler.cs
@@ -0,0 +1,106 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Exceptions;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Results;
+using SIS.WebServer.Routing;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SIS.WebServer
+{
+    public class ConnectionHandler
+    {
+        private readonly Socket client;
+        private readonly IServerRoutingTable serverRoutingTable;
+
+        public ConnectionHandler(Socket client, IServerRoutingTable serverRoutingTable)
+        {
+            CoreValidator.ThrowIfNull(client, nameof(client));
+            CoreValidator.ThrowIfNull(serverRoutingTable, nameof(serverRoutingTable));
+
+            this.client = client;
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public void ProcessRequest()
+        {
+            try
+            {
+                var httpRequest = this.ReadRequest();
+
+                if (httpRequest != null)
+                {
+                    Console.WriteLine($"Processing {httpRequest.RequestMethod} {httpRequest.Path}");
+
+                    var httpResponce = this.HandleRequest(httpRequest);
+
+                    this.PrepareResponce(httpResponce);
+                }
+            }
+            catch (BadRequestException e)
+            {
+                this.PrepareResponce(new TextResult(e.ToString(), HttpResponseStatusCode.BadRequest));
+            }
+            catch (Exception e)
+            {
+                this.PrepareResponce(new TextResult(e.ToString(), HttpResponseStatusCode.InternalServrError));
+            }
+
+            this.client.Shutdown(SocketShutdown.Both);
+        }
+
+        private IHttpRequest ReadRequest()
+        {
+            var result = new StringBuilder();
+            var data = new ArraySegment<byte>(new byte[1024]);
+
+            while (true)
+            {
+                int numberOfBytesRead = this.client.Receive(data.Array, SocketFlags.None);
+
+                if (numberOfBytesRead == 0)
+                {
+                    break;
+                }
+
+                var bytesAsString = Encoding.UTF8.GetString(data.Array, 0, numberOfBytesRead);
+                result.Append(bytesAsString);
+
+                if (numberOfBytesRead < 1023)
+                {
+                    break;
+                }
+            }
+
+            if (result.Length == 0)
+            {
+                return null;
+            }
+
+            return new HttpRequest(result.ToString());
+
+        }
+
+        private IHttpResponse HandleRequest(IHttpRequest httpRequest)
+        {
+            if (!this.serverRoutingTable.Contains(httpRequest.RequestMethod, httpRequest.Path))
+            {
+                return new TextResult
+                    ($"Route with method {httpRequest.RequestMethod} and path \"{httpRequest.Path}\" not found", 
+                    HttpResponseStatusCode.NotFound);
+            }
+
+            return this.serverRoutingTable.Get(httpRequest.RequestMethod, httpRequest.Path).Invoke(httpRequest);
+        }
+
+        private void PrepareResponce(IHttpResponse httpResponse)
+        {
+            byte[] byteSegments = httpResponse.GetBytes();
+
+            this.client.Send(byteSegments, SocketFlags.None);
+        }
+    }
+}
