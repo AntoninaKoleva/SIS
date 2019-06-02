ff1d2731a028f97c038a1b096181a4208b9ca2f2
Antonina <antonina.koleva1@gmail.com>
2019-06-02 17:22:32 +0300

    Added Html, Redirect and Text results.

---
+++ SIS.WebServer/Results/TextResult.cs
@@ -0,0 +1,28 @@
+﻿using SIS.HTTP.Enums;
+using SIS.HTTP.Header;
+using SIS.HTTP.Responses;
+using System.Text;
+
+namespace SIS.WebServer.Results
+{
+    public class TextResult : HttpResponse
+    {
+        public TextResult(string content, 
+            HttpResponseStatusCode statusCode, 
+            string contentType = "text/plain; charset=utf-8") 
+            : base(statusCode)
+        {
+            this.Headers.AddHeader(new HttpHeader(HttpHeader.ContentType, contentType));
+            this.Content = Encoding.UTF8.GetBytes(content);
+        }
+
+        public TextResult(byte[] content, 
+            HttpResponseStatusCode statusCode, 
+            string contentType = "text/plain; charset=utf-8")
+            : base(statusCode)
+        {
+            this.Content = content;
+            this.Headers.AddHeader(new HttpHeader(HttpHeader.ContentType, contentType));
+        }
+    }
+}
