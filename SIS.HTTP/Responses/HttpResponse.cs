c8713eeb363fc5b08bfb3df32c7166460f6663d4
Antonina <antonina.koleva1@gmail.com>
2019-06-02 14:54:33 +0300

    Added HttpResponse.

---
+++ SIS.HTTP/Responses/HttpResponse.cs
@@ -0,0 +1,67 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Header;
+using System.Text;
+
+namespace SIS.HTTP.Responses
+{
+    public class HttpResponse : IHttpResponse
+    {
+        public HttpResponse()
+        {
+            this.Headers = new HttpHeaderCollection();
+            this.Content = new byte[0];
+        }
+
+        public HttpResponse(HttpResponseStatusCode statusCode) : this()
+        {
+            CoreValidator.ThrowIfNull(statusCode, nameof(statusCode));
+            this.StatusCode = statusCode;
+        }
+
+        public HttpResponseStatusCode StatusCode { get; set; }
+
+        public IHttpHeaderCollection Headers { get; private set; }
+
+        public byte[] Content { get; set; }
+
+        public void AddHeader(HttpHeader header)
+        {
+            CoreValidator.ThrowIfNull(header, nameof(header));
+            this.Headers.AddHeader(header);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] httpResponseBytesWithoutBody = Encoding.UTF8.GetBytes(this.ToString());
+
+            byte[] httpResponseBytesWithBody = new byte[httpResponseBytesWithoutBody.Length + this.Content.Length];
+
+            for (int i = 0; i < httpResponseBytesWithoutBody.Length; i++)
+            {
+                httpResponseBytesWithBody[i] = httpResponseBytesWithoutBody[i];
+            }
+
+            for (int i = 0; i < httpResponseBytesWithBody.Length - httpResponseBytesWithoutBody.Length; i++)
+            {
+                httpResponseBytesWithBody[i + httpResponseBytesWithoutBody.Length] = this.Content[i];
+            }
+
+            return httpResponseBytesWithBody;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"{GlobalConstants.HttpOneProtocolFragment} {(int)this.StatusCode} {this.StatusCode.ToString()}")
+                .Append(GlobalConstants.HttpNewLine)
+                .Append(this.Headers)
+                .Append(GlobalConstants.HttpNewLine);
+
+            sb.Append(GlobalConstants.HttpNewLine);
+
+            return sb.ToString();
+        }
+    }
+}
